f29c25413bdc08c6956667868bc71ef902056626
Binti Bhatt <bintibhatt.zuru@gmail.com>
2024-01-11 14:11:20 +0530

    Linking and Navigating

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,3 +1,34 @@
+import Content from "../components/content";
+import Link from "next/link";
+
 export default function Page() {
-  return <h1>Hello, Dashboard Page!</h1>;
+  return (
+    <>
+      <h1>Hello, Dashboard Page!</h1>
+      <Link href="/">
+        <button
+          type="submit"
+          class="rounded-md bg-indigo-900 px-4 py-1.5 text-md font-semibold leading-6 text-white shadow-sm hover:bg-indigo-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Go to Home Page
+        </button>
+      </Link>
+      <Link href="/dashboard#settings">
+        <button
+          type="submit"
+          class="rounded-md bg-indigo-900 px-4 py-1.5 text-md font-semibold leading-6 text-white shadow-sm hover:bg-indigo-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Go to Settings
+        </button>
+      </Link>
+      <div>
+        {/* <Content />
+        <Content />
+        <Content /> */}
+      </div>
+      <div id="settings">
+        <h1>Settings</h1>
+      </div>
+    </>
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,17 @@
+import Link from "next/link";
+
 export default function Page() {
-  return <h1>Hello, Home page!</h1>;
+  return (
+    <>
+      <h1>Hello, Home page!</h1>
+      <Link href="/dashboard">
+        <button
+          type="submit"
+          class="rounded-md bg-indigo-900 px-4 py-1.5 text-md font-semibold leading-6 text-white shadow-sm hover:bg-indigo-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Go to Dashboard
+        </button>
+      </Link>
+    </>
+  );
 }
